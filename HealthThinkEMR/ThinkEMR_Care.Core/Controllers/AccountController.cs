e7aba1c1aeb428208c90a647a70b80dbe1916bba
Haridas-Dhulgande <dhulgandeharidas83@gmail.com>
2023-11-03 13:20:58 +0530

    Add Authentication Pages ForgotPass,EnterOTP,ResetPass,Session

--- HealthThinkEMR/ThinkEMR_Care.Core/Controllers/AccountController.cs
+++ HealthThinkEMR/ThinkEMR_Care.Core/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ namespace ThinkEMR_Care.Core.Controllers
 {
     public class AccountController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
@@ -14,6 +15,10 @@ namespace ThinkEMR_Care.Core.Controllers
 
         public IActionResult Login()
         {
+            if(HttpContext.Session.GetString("JWToken") != null)
+            {
+                return RedirectToAction("Index", "AdminDashboard");
+            }
             return View();
         }
 
@@ -35,7 +40,6 @@ namespace ThinkEMR_Care.Core.Controllers
                             if (apiResponse.IsSuccess)
                             {
                                 HttpContext.Session.SetString("JWToken", apiResponse.Responce);
-
                                 return RedirectToAction("Index", "AdminDashboard");
                             }
                             else
@@ -54,6 +58,261 @@ namespace ThinkEMR_Care.Core.Controllers
             else
             {
                 return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword (EmailModel emailmodel)
+        {
+            if(!ModelState.IsValid)
+            {
+                TempData["errormessage"] = "Please enter a valid email address.";
+                return View(emailmodel);
+            }
+            else
+            {
+                try
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        StringContent stringContent = new StringContent(JsonConvert.SerializeObject(emailmodel), Encoding.UTF8, "application/json");
+                        string apiUrl = $"https://localhost:7286/api/AuthenticationService/ForgotPassword?email={Uri.EscapeDataString(emailmodel.Email)}";
+
+                        using (var response = await httpClient.PostAsync(apiUrl, stringContent))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string ResetPassResponce = HttpContext.Session.GetString("JWToken");
+                                string responseContent = await response.Content.ReadAsStringAsync();
+                                var apiResponse = JsonConvert.DeserializeObject<PasswordResetResponse>(responseContent);
+
+                                PasswordResetResponse apiResponseData = new PasswordResetResponse
+                                {
+                                    Status = apiResponse.Status,
+                                    Otp = apiResponse.Otp,
+                                    Token = apiResponse.Token,
+                                    Email = apiResponse.Email,
+                                    Message = apiResponse.Message
+                                };
+                                string apiResponseJson = JsonConvert.SerializeObject(apiResponseData);
+                                HttpContext.Session.SetString("ApiResponse", apiResponseJson);
+
+
+
+                                if (apiResponse.Status)
+                                {
+                                    HttpContext.Session.SetString("ResetPassword", apiResponse.Token);
+                                    return RedirectToAction("EnterOTP", "Account");
+                                }
+                                else
+                                {
+                                    TempData["errormessage"] = "Incorrect Email";
+                                }
+                            }
+                            else
+                            {
+                                TempData["errormessage"] = "An error occurred while processing the login request.";
+                            }
+                            return RedirectToAction("Login", "Account");
+                        }
+
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        
+        public async Task<IActionResult> EnterOTP()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EnterOTP([FromBody] UserOTP userOTP)
+        {
+            if(!ModelState.IsValid)
+            {
+                TempData["errormessage"] = "Please check the provided information.";
+                return Json(new { redirectTo = "Error" });
+            }
+            else
+            {
+                try
+                {
+                    var UserOTPData = userOTP.InputOTP;
+                    if (!string.IsNullOrEmpty(UserOTPData))
+                    {
+                        string apiResponseJsonExistSession = HttpContext.Session.GetString("ApiResponse");
+                        string ResendOTPSessionCreate = HttpContext.Session.GetString("ResendOTP");
+
+                        if (!string.IsNullOrEmpty(ResendOTPSessionCreate) )
+                        {
+                            PasswordResetResponse apiResponse = JsonConvert.DeserializeObject<PasswordResetResponse>(ResendOTPSessionCreate);
+
+                            if (apiResponse != null)
+                            {
+                                if (apiResponse.Otp == userOTP.InputOTP)
+                                {
+                                    return Json(new { redirectTo = "ResetPassword" });
+                                }
+                                else
+                                {
+                                    TempData["errormessage"] = "OTP did not match.";
+                                    return Json(new { redirectTo = "Error" });
+                                }
+                            }
+                        }else if(!string.IsNullOrEmpty(apiResponseJsonExistSession))
+                        {
+                            PasswordResetResponse apiResponseExistSession = JsonConvert.DeserializeObject<PasswordResetResponse>(apiResponseJsonExistSession);
+
+                            if (apiResponseExistSession != null)
+                            {
+                                if (apiResponseExistSession.Otp == userOTP.InputOTP)
+                                {
+                                    return Json(new { redirectTo = "ResetPassword" });
+                                }
+                                else
+                                {
+                                    TempData["errormessage"] = "OTP did not match.";
+                                    return Json(new { redirectTo = "Error" });
+                                }
+                            }
+                        }
+                    }
+                    TempData["errormessage"] = "Please Try Again..!!";
+                    return View(userOTP);
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+
+        public async Task<IActionResult> ResetPassword()
+        {
+            return  View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordData resetPassword)
+        {
+            try
+            {
+                if (resetPassword.Password != null && resetPassword.ConfirmPassword != null && resetPassword.Password == resetPassword.ConfirmPassword)
+                {
+                    string apiResponseJson = HttpContext.Session.GetString("ApiResponse");
+                    if (!string.IsNullOrEmpty(apiResponseJson))
+                    {
+                        ResetPasswordData apiResponse = JsonConvert.DeserializeObject<ResetPasswordData>(apiResponseJson);
+                        if (apiResponse != null)
+                        {
+                            var RequestBody = new ResetPasswordData
+                            {
+                                Password = resetPassword.Password,
+                                ConfirmPassword = resetPassword.ConfirmPassword,
+                                Email = apiResponse.Email,
+                                Token = apiResponse.Token,
+                                OTP = apiResponse.OTP,
+                            };
+
+                            using (var httpClient = new HttpClient())
+                            {
+                                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(RequestBody), Encoding.UTF8, "application/json");
+                                using (var response = await httpClient.PostAsync("https://localhost:7286/api/AuthenticationService/Reset-Password", stringContent))
+                                {
+                                    if (response.IsSuccessStatusCode)
+                                    {
+                                        TempData["Success"] = "Password Updated Successfully";
+                                        return RedirectToAction("Login", "Account");
+                                    }
+                                    else
+                                    {
+                                        TempData["errormessage"] = "Something went wrong with the API request.";
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            TempData["errormessage"] = "Invalid API response data";
+                        }
+                    }
+                    else
+                    {
+                        TempData["errormessage"] = "Password and ConfirmPassword do not match";
+                    }
+                }
+                else
+                {
+                    TempData["errormessage"] = "Password and ConfirmPassword should be same";
+                    return View(resetPassword);
+                }
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResendOTP()
+        {
+            try
+            {
+                string apiResponseJson = HttpContext.Session.GetString("ApiResponse");
+
+                if (string.IsNullOrEmpty(apiResponseJson))
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+           
+                PasswordResetResponse apiResponse = JsonConvert.DeserializeObject<PasswordResetResponse>(apiResponseJson);
+
+                using (var httpClient = new HttpClient())
+                {
+                    StringContent stringContent = new StringContent(JsonConvert.SerializeObject(apiResponse), Encoding.UTF8, "application/json");
+                    string apiUrl = $"https://localhost:7286/api/AuthenticationService/ResendOTP?email={Uri.EscapeDataString(apiResponse.Email)}";
+
+                    using (var response = await httpClient.PostAsync(apiUrl, stringContent))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string ResendOTPSessionCreate = HttpContext.Session.GetString("ResendOTP");
+                            string responseContent = await response.Content.ReadAsStringAsync();
+                            var ResendOTP = JsonConvert.DeserializeObject<ResendOTPSessionData>(responseContent);
+
+                            HttpContext.Session.SetString("ResendOTP", JsonConvert.SerializeObject(new
+                            {
+                                ResendOTP.Status,
+                                ResendOTP.Otp,
+                                ResendOTP.Token,
+                                ResendOTP.Email,
+                                ResendOTP.Message
+                            }));
+
+                            return RedirectToAction("EnterOTP", "Account");
+                        }
+                    }
+                }
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
---
+++ HealthThinkEMR/ThinkEMR_Care.Core/Models/EmailModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThinkEMR_Care.Core.Models
+{
+    public class EmailModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+    }
+}
