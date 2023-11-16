961c59dd838bfb407e6faab3c593e28b1d78fd5e
Haridas-Dhulgande <dhulgandeharidas83@gmail.com>
2023-11-16 14:50:31 +0530

    1. Add UserSetting Controller and view 2. Display all user ehich is availabe in DB 3. Create new user and show same on the view

---
+++ HealthThinkEMR/ThinkEMR_Care.Core/Models/AdminProfileDetails.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThinkEMR_Care.Core.Models
+{
+    public class AdminProfileDetails
+    {
+            [Required(ErrorMessage = "User Name is Required")]
+            public string UserName { get; set; }
+
+            [Required(ErrorMessage = "Email is Required")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Password is Required")]
+            public string Password { get; set; }
+
+            [Required(ErrorMessage = "First Name is Required")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Last Name is Required")]
+            public string LastName { get; set; }
+
+            [Required(ErrorMessage = "Contact Number is Required")]
+            public string PhoneNumber { get; set; }
+            public DateTime? LastLogin { get; set; }
+            public bool Status { get; set; }
+            public bool IsDeleted { get; set; }
+            public string? ProfileImage { get; set; }
+
+            [Required(ErrorMessage = "Role is Required")]
+            public string Role { get; set; }
+    }
+}
---
+++ HealthThinkEMR/ThinkEMR_Care.Core/Models/ResponceContent.cs
@@ -0,0 +1,8 @@
+﻿namespace ThinkEMR_Care.Core.Models
+{
+    public class ResponceContent
+    {
+        public string? status { get; set; }
+        public string? Message { get; set; }
+    }
+}
