a4e45b79e7f807f87ca77027d3e41ced6bac1964
Haridas-Dhulgande <dhulgandeharidas83@gmail.com>
2023-11-21 12:26:32 +0530

    1. Add and bind the change password endpoint 2. Add requried validation 3. Resloved modalpopup issue

--- HealthThinkEMR/ThinkEMR_Care.Core/Models/ChangeUserPassword.cs
+++ HealthThinkEMR/ThinkEMR_Care.Core/Models/ChangeUserPassword.cs
@@ -1,9 +1,18 @@
-﻿namespace ThinkEMR_Care.Core.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThinkEMR_Care.Core.Models
 {
     public class ChangeUserPassword
     {
+        [Required(ErrorMessage = "Please enter the current password.")]
         public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "Please enter the new password.")]
         public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Please confirm the new password.")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirm password do not match.")]
         public string ConformNewPassword { get; set; }
     }
+
 }
